23676a075fb7781f7aa69d8b8c752fc49497780d
Alexandr <alexmen00008@gmail.com>
2025-09-25 07:34:00 +0300

    complete lesson-04-02

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, содержащий только уникальные элементы из исходного массива.
+Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, 
+содержащий только уникальные элементы из исходного массива.
 
 **Входные данные:**
 
@@ -15,4 +16,13 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (includesElement(newArray, array[i])) {
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
